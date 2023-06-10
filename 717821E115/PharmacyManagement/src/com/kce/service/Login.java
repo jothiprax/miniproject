db643a957f3d761719c587c17bd78b40e642a880
Dhanushkciean <136176670+Dhanushkciean@users.noreply.github.com>
2023-06-10 23:48:11 +0530

    Mini Project has been completed

---
+++ 717821E115/PharmacyManagement/src/com/kce/service/Login.java
@@ -0,0 +1,14 @@
+package com.kce.service;
+
+import com.kce.bean.LoginAccess;
+
+public class Login extends LoginAccess{
+
+	public Login(String userName, String password) {
+		super(userName, password);
+
+	}
+
+}
+
+
