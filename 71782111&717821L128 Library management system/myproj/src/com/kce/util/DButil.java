cad32a57d485c8447826e6371c875807da46d30c
Elamathi81 <136800193+Elamathi81@users.noreply.github.com>
2023-06-16 15:37:31 +0530

    Update DButil.java

--- 71782111&717821L128 Library management system/myproj/src/com/kce/util/DButil.java
+++ 71782111&717821L128 Library management system/myproj/src/com/kce/util/DButil.java
@@ -1,9 +1,9 @@
 package com.kce.util;
 import java.sql.*;
-//import java.sql.DriverManager;
+
 import java.sql.SQLException;
 
-//import com.mysql.jdbc.Connection;
+
 
 public class DButil {
 	public static Connection getConnection() throws SQLException {
