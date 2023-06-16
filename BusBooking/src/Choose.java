c017cb1143b27582153e3d5d2bcb381ac1443d6b
HARISH RAGAVENDRA D <136805178+harishragavendra1810@users.noreply.github.com>
2023-06-16 21:52:53 +0530

    Add files via upload

--- BusBooking/src/Choose.java
+++ BusBooking/src/Choose.java
@@ -1,7 +1,8 @@
 public class Choose { 
 	static int choose;
 	public void choose() {
-	   System.out.println("Type 1 for Booking: ");
-	   System.out.println("Type 2 for Add Bus: ");
+	   System.out.println("Type 1 for Booking");
+	   System.out.println("Type 2 for Add Bus");
+	   System.out.println("Type 3 for CustomerDetails ");
 	}
 }
