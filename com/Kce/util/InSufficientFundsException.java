a6884bdfdd2fc2b848ad6ae91be85e72b14fe232
Rajesh-Chandrasekaran <136782022+Rajesh-Chandrasekaran@users.noreply.github.com>
2023-06-16 13:32:45 +0530

    Delete com/Kce directory

--- com/Kce/util/InSufficientFundsException.java
+++
@@ -1,8 +0,0 @@
-package com.Kce.util;
-import java.lang.*;
-public class InSufficientFundsException extends Exception {
-    //toString() to return Insufficient Exception
-    public String toString() {
-        return "INSUFFICIENT FUNDS";
-    }
-}
--- com/Kce/util/MTPINMismatchException.java
+++
@@ -1,9 +0,0 @@
-package com.Kce.util;
-import java.lang.*;
-public class MTPINMismatchException extends Exception{
-    //toString() to return wrong MIPIN
-    @Override
-    public String toString() {
-        return "Wrong MTPIN";
-    }
-}
