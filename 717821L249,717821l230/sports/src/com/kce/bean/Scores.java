b699446d1f8383ae369498ca340089cd3c1d5e83
717821lL249 <136723359+717821lL249@users.noreply.github.com>
2023-06-19 21:55:59 +0530

    Add files via upload

---
+++ 717821L249,717821l230/sports/src/com/kce/bean/Scores.java
@@ -0,0 +1,37 @@
+package com.kce.bean;
+import com.kce.bean.Members;
+import com.kce.bean.Schedules;
+public class Scores extends Members{
+
+					 public String name;
+					 public int id;
+					 public int score;
+
+				    public String getName() {
+				        return name;
+				    }
+
+				    public void setName(String name) {
+				        this.name = name;
+				    }
+
+				    public int getId() {
+				        return id;
+				    }
+
+				    public void setId(int id) {
+				        this.id = id;
+				    }
+
+				    public int getScore() {
+				        return score;
+				    }
+
+				    public void setScore(int score) {
+				        this.score = score;
+				    }
+
+				    
+				}
+
+
