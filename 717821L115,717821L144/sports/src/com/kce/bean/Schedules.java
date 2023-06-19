b33f976f89ab2f29f1de6dd11a0fa54b2263ce14
Santhosh19404 <136779123+Santhosh19404@users.noreply.github.com>
2023-06-19 22:03:03 +0530

    Add files via upload

---
+++ 717821L115,717821L144/sports/src/com/kce/bean/Schedules.java
@@ -0,0 +1,36 @@
+package com.kce.bean;
+import com.kce.bean.Members;
+public class Schedules extends Members {
+	
+					 public String name;
+					 public int id;
+					 public int time;
+
+				    public String getName() {
+				        return name;
+				    }
+
+				    public void setName(String name) {
+				        this.name = name;
+				    }
+
+				    public int getId() {
+				        return id;
+				    }
+
+				    public void setId(int id) {
+				        this.id = id;
+				    }
+
+				    public int getTime() {
+				        return time;
+				    }
+
+				    public void setTime(int time) {
+				        this.time = time;
+				    }
+
+				    
+				}
+
+
