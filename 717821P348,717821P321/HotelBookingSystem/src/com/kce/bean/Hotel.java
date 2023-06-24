0bee87ddfe71d0ac3dcd48080686bfdc55bc126c
sarveswaran01 <137636275+sarveswaran01@users.noreply.github.com>
2023-06-24 20:45:29 +0530

    Miniproject completed

---
+++ 717821P348,717821P321/HotelBookingSystem/src/com/kce/bean/Hotel.java
@@ -0,0 +1,36 @@
+package com.kce.bean;
+
+public class Hotel {
+    private int id;
+    private String name;
+    private String location;
+
+    public Hotel(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
