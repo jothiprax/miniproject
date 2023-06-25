1028696b0fa98b78c6896556e0959453ba47ae44
ABISHEKGR04 <122039584+ABISHEKGR04@users.noreply.github.com>
2023-06-25 23:42:56 +0530

    Add files via upload

---
+++ 717821P202,717821P233/Employee Management/src/com/kce/dao/EmployeeConnection.java
@@ -0,0 +1,90 @@
+package com.kce.dao;
+
+import com.kce.bean.EmployeeDataBase;
+import java.sql.*;
+public class EmployeeConnection {
+	private PreparedStatement create;
+	private PreparedStatement select;
+	private PreparedStatement update;
+	private PreparedStatement delete;
+	public EmployeeConnection() throws ClassNotFoundException {
+	try{
+	String url="jdbc:oracle:thin:@localhost:1521:xe";
+	String user="System";
+	String password="abishek2004";
+	Class.forName("oracle.jdbc.driver.OracleDriver");
+	Connection con=DriverManager.getConnection(url,user,password);
+	create=con.prepareStatement("insert into employees values(?,?,?,?)");
+	select=con.prepareStatement("Select * from employees where eid=?");
+	update=con.prepareStatement("update employees set ename=?,degree=?,salary=? where id=?");
+	delete=con.prepareStatement("delete from employees where id=?");
+	}
+	catch (SQLException e) {
+	System.out.println("SQL Exception occurred.....");
+	}
+	}
+	public void createEmployee(EmployeeDataBase employee) {
+	try {
+	create.setInt(1,employee.getEid());
+	create.setString(2,employee.getEname());
+	create.setString(3,employee.getDegree());
+	create.setDouble(4,employee.getSalary());
+	create.executeUpdate();
+	System.out.println("Employee created successfully");
+	}
+	catch(SQLException e) {
+	System.out.println("SQL Exception....");
+	}
+	}
+	public EmployeeDataBase readEmployee(int eid) {
+	try {
+	select.setInt(1,eid);
+	ResultSet rs=select.executeQuery();
+	if(rs.next()){
+	String ename=rs.getString("ename");
+	String degree=rs.getString("degree");
+	double salary=rs.getDouble("salary");
+	return new EmployeeDataBase(eid,ename,degree,salary);
+	}
+	}
+	catch(SQLException e) {
+	System.out.println("SQL Exception....");
+	}
+	return null;
+
+	}
+	public void updateEmployee(EmployeeDataBase employee) {
+	try {
+	update.setInt(1,employee.getEid());
+	update.setString(2,employee.getEname());
+	update.setString(3,employee.getDegree());
+	update.setDouble(4,employee.getSalary());
+	int rowsaffected=update.executeUpdate();
+	if(rowsaffected>0) {
+	System.out.println("Employee updated successfully");
+	}
+	else {
+	System.out.println("Sorry,Not Found....Please Try again later!!!");
+	}
+	}
+	catch(SQLException e) {
+	System.out.println("SQL Exception....");
+	}
+	}
+	public void deleteEmployee(int eid){
+	try {
+	delete.setInt(1, eid);
+	int rowsdeleted=delete.executeUpdate();
+	if(rowsdeleted>0) {
+	System.out.println("Employee deleted successfully");
+	}
+	else {
+	System.out.println("Sorry,Not Found....Please Try again later!!!");
+	}
+	}
+	catch(SQLException e) {
+	System.out.println("SQL Exception....");
+	}
+
+	}
+}
