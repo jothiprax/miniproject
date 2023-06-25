b7637541e3712c6907cfb4c1baa9f4cbc3d0b44e
Rajkumar2210 <118796055+Rajkumar2210@users.noreply.github.com>
2023-06-25 19:51:18 +0530

    Add files via upload

---
+++ 717821d134 & 717821d133/src/com/kce/stud/service/StaffSignIn.java
@@ -0,0 +1,13 @@
+package com.kce.stud.service;
+import com.kce.stud.bean.*;
+class StaffSignIn extends LoginPage {
+public StaffSignIn(String username, String password, String staffId) {
+	super( username,password,staffId);
+}
+
+public StaffSignIn() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+
+}
