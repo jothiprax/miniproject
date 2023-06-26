6f16c38f22084022f76efc4fe2b56d52f6f12f34
ParveshRaffiqAhmed <137816125+ParveshRaffiqAhmed@users.noreply.github.com>
2023-06-26 22:18:10 +0530

    Add files via upload

---
+++ schoolmanagementsystem/src/com/kce/bean/Student.java
@@ -0,0 +1,67 @@
+package com.kce.bean;
+
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+    private String address;
+    private String phone;
+
+    public Student(int id, String name, int age, String gender, String address, String phone) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
